ad594e8a6d5feff2692470065bfee38f28466a42
PARK9898 <wjddjs21@naver.com>
2023-09-14 20:32:36 +0900

    "연관관계 적용하여 리팩토링"

--- src/main/java/com/group/libraryapp/service/book/BookService.java
+++ src/main/java/com/group/libraryapp/service/book/BookService.java
@@ -42,8 +42,8 @@ public class BookService {
 		}
 		User user = userRepository.findByName(request.getUserName())
 			.orElseThrow(() -> new IllegalArgumentException());
-
-		userLoanHistoryRepository.save(new UserLoanHistory(user.getId(),book.getName(),false));
+		user.loanBook(book.getName()); //cascade 옵션에 의해 user와 user가 가지고 있는 리스트안에 새로운 히스토리가 만들어지고 트랜잭션이 끝날때 새로운 연관관계가 맺어진걸 체크해서 새로운 히스토리를 db에 저장
+		//userLoanHistoryRepository.save(new UserLoanHistory(user,book.getName()));
 		//4. 유저 정보를 가져온다
 		//5. 유저 정보와 책 정보를 기반으로 히스토리 작성
 	}
@@ -51,12 +51,12 @@ public class BookService {
 	public void returnBook(BookReturnRequest request) {
 		User user = userRepository.findByName(request.getUserName())
 			.orElseThrow(IllegalArgumentException::new);
-
-		UserLoanHistory userLoanHistory = userLoanHistoryRepository.findByUserIdAndBookName(user.getId(),request.getBookName())
-			.orElseThrow(IllegalArgumentException::new);
-
-
-		userLoanHistory.doReturn(); // 이런 이제 자신의 필드 값 자체를 바꾸는 행위는 보통 엔티티 딴에서 메소드를 만들어준다 왜냐면 외부에서 setter 나 getter를 쓰는것은 안좋기 때문
+		user.returnBook(request.getBookName());
+		// UserLoanHistory userLoanHistory = userLoanHistoryRepository.findByUserIdAndBookName(user.getId(),request.getBookName())
+		// 	.orElseThrow(IllegalArgumentException::new);
+		//
+		//
+		// userLoanHistory.doReturn(); // 이런 이제 자신의 필드 값 자체를 바꾸는 행위는 보통 엔티티 딴에서 메소드를 만들어준다 왜냐면 외부에서 setter 나 getter를 쓰는것은 안좋기 때문
 
 		//영속성 컨텍스트 떄문에 자동 업데이트 때문에 userLoanHistoryRepository.save(history) 안해줘도 된다
 	}
