01ea7ae2884e0a41830b3e4cb6c2255eebc3e6ea
PARK9898 <wjddjs21@naver.com>
2023-09-14 20:32:55 +0900

    "연관관계 적용하에 따른 도메인에 서비스 로직 추가"

--- src/main/java/com/group/libraryapp/domain/user/User.java
+++ src/main/java/com/group/libraryapp/domain/user/User.java
@@ -1,10 +1,17 @@
 package com.group.libraryapp.domain.user;
 
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.OneToMany;
+
+import com.group.libraryapp.domain.user.loanhistory.UserLoanHistory;
 
 @Entity // 스프링이 User 객체와 user 테이블을 같은것으로 바라본다 저장되고 관리되어야하는 데이터
 public class User {
@@ -14,7 +21,8 @@ public class User {
 	@Column(nullable = false,length =20 ,name = "name") //객체의 필드와 Table의 필드를 매핑한다 이름 같으면 name = "name" 생략 가능
 	private String name;
 	private Integer age; // age는 Integer와 int가 동일하고  null이 들어가도 되니까 Colum 사용하지 않아도 됨
-
+	@OneToMany(mappedBy = "user",cascade = CascadeType.ALL,orphanRemoval = true) // 자신이 연관관계의 주인이 아님
+	private List<UserLoanHistory> userLoanHistories = new ArrayList<>();
 	protected User() { // jpa를 사용하기 위해서는 기본 생성자가 필요하다
 	}
 
@@ -38,5 +46,18 @@ public class User {
 
 	public Integer getAge() {
 		return age;
+	}
+
+	public void loanBook(String bookName) {
+		this.userLoanHistories.add(new UserLoanHistory(this,bookName));
+	}// 도메인 계츨에 비즈니스 로직이 들어갔다고 표현하기도 한다
+	//이미 반납한것도 히스토리에 저장되어있을텐데
+	// 만약 히스토리에 책이름 유저이름 반납완료 0 이면 어떡할건데?
+	public void returnBook(String bookName) {
+		UserLoanHistory targetHistory = this.userLoanHistories.stream()
+			.filter(history -> history.getBookName().equals(bookName))
+			.findFirst()
+			.orElseThrow(IllegalAccessError::new);  //findFist()는 옵셔널로 반홚나다
+		targetHistory.doReturn();
 	}
 }
--- src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
+++ src/main/java/com/group/libraryapp/domain/user/loanhistory/UserLoanHistory.java
@@ -4,6 +4,9 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
+
+import com.group.libraryapp.domain.user.User;
 
 @Entity
 public class UserLoanHistory {
@@ -11,15 +14,16 @@ public class UserLoanHistory {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id = null;
 
-	private long userId;
+	@ManyToOne // 내가 다수이고 너가 하나 대출기록은 여러개이고 그 대출기록을 소유하고 있는 사용자는 한명이다
+	private User user;
 
 	protected UserLoanHistory() {
 
 	}
-	public UserLoanHistory(long userId, String bookName, boolean isReturn) {
-		this.userId = userId;
+	public UserLoanHistory(User user, String bookName) {
+		this.user = user;
 		this.bookName = bookName;
-		this.isReturn = isReturn;
+		this.isReturn = false;
 	}
 
 	public void doReturn() {
@@ -27,6 +31,9 @@ public class UserLoanHistory {
 	}
 	private String bookName;
 
+	public String getBookName() {
+		return bookName;
+	}
 	private boolean isReturn; // boolean으로 처리하면 tinyint에 잘 매핑된다
 
 }
